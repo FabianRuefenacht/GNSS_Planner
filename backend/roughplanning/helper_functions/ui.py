998abeabaafdb9306bebd0227b896ccefb2d0851
Fabian Rüfenacht <f.ruefenacht@outlook.com>
2024-07-03 17:52:02 +0200

    code documentation

--- backend/roughplanning/helper_functions/ui.py
+++ backend/roughplanning/helper_functions/ui.py
@@ -1,9 +1,9 @@
 from PyQt5.QtWidgets import QLabel, QProgressBar, QApplication
 
 def update_progresBar(bar: QProgressBar, label: QLabel, value: int, text: str) -> None:
-    bar.setValue(value)
-    label.setText(text)
+    bar.setValue(value) # update progressBar
+    label.setText(text) # update text-hint for user
 
-    QApplication.processEvents()
+    QApplication.processEvents() # ensure function is terminated before next action in main is called
 
     return
